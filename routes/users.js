e3be776de944d1c61565f9360b536b9203029dce
Ewerton Luna <ewerton.al@gmail.com>
2020-07-14 11:37:23 -0300

    Refactor error handlers for user's routes

--- routes/users.js
+++ routes/users.js
@@ -13,25 +13,64 @@ router.get('/users', asyncHandler((req, res) => {
 router.post('/users', asyncHandler(async (req, res, next) => {
   try {
     const user = req.body;
+    const { password } = user;
+    // If password is truthy, hash user password. Otherwise, do nothing to it.
+    user.password = password ? bcryptjs.hashSync(user.password) : password;
+
     await User.create(user);
     res.status(201).json({user});
   } catch (error) {
-    let errorMessages = [];
-
+    const message = [];
     // If validation fails (Sequelize level error)...
     if (error.name === 'SequelizeValidationError') {
-      error.errors.map(err => errorMessages.push(err.message));
+      error.errors.map(err => message.push(err.message));
     
     // If emailAddress filed isn't unique (SQL level error)...
     } else if (error.name === 'SequelizeUniqueConstraintError') {
-      errorMessages.push("The provided email isn't unique");
+      message.push("The provided email isn't unique");
     }
 
-    if (errorMessages.length > 0) {
-      res.status(400).json({errors: errorMessages});
-    } else {
-      throw error;
+    if (message.length > 0) {
+      error.message = message;
+      error.status = 400;
     }
+    throw error;
+  }
+}));
+
+router.put('/users/:id', asyncHandler(async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+
+    if (user) {
+      console.log(user);
+      const updatedFields = req.body;
+      const { password } = updatedFields;
+      
+      updatedFields.password = password ? bcryptjs.hashSync(password) : password;
+      
+      await user.update(updatedFields);
+      res.status(204).end();
+    } else {
+      res.status(404).json({message: 'User not found'});
+    }
+
+  } catch (error) {
+    const message = [];
+    // If validation fails (Sequelize level error)...
+    if (error.name === 'SequelizeValidationError') {
+      error.errors.map(err => message.push(err.message));
+    
+    // If emailAddress filed isn't unique (SQL level error)...
+    } else if (error.name === 'SequelizeUniqueConstraintError') {
+      message.push("The provided email isn't unique");
+    }
+
+    if (message.length > 0) {
+      error.message = message;
+      error.status = 400;
+    }
+    throw error;
   }
 }));
 
