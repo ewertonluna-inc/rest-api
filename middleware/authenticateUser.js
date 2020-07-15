17c95052fa78753d53aa6b7817ffd1390111996f
Ewerton Luna <ewerton.al@gmail.com>
2020-07-15 10:53:35 -0300

    Create authenticateUser middleware function

--- middleware/authenticateUser.js
+++ middleware/authenticateUser.js
@@ -1,5 +1,36 @@
 const auth = require('basic-auth');
+const bcryptjs = require('bcryptjs');
 
-module.exports = (req, res, next) => {
-  const credentials = auth(req);
+
+module.exports = ( model ) => {
+  return (
+    async (req, res, next) => {
+      let message;
+      const credentials = auth(req);
+      
+      if (credentials) {
+        const user = await model.findOne({ where: { emailAddress: credentials.name } });
+        if (user) {
+          const authenticated = bcryptjs.compareSync(credentials.pass, user.password);
+          if (authenticated) {
+            console.log(`Authentication successfull for user ${user.emailAddress}`);
+            req.currentUser = user;
+          } else {
+            message = `Authentication failed for user ${user.emailAddress}`;
+          }
+        } else {
+          message = `User's email '${credentials.name}' not found`;
+        }
+      } else {
+        message = 'Authorization header not found';
+      }
+
+      if (message) {
+        console.warn(message);
+        res.status(401).json({ message: 'Access Denied' });
+      } else {
+        next();
+      }
+    }
+  );
 }
